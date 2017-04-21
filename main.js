3900873d50bdf9cbe32dc5f7deca38dcbb2a6b32
TheFreddoT <freddytuxworth@gmail.com>
2017-04-21 17:43:05 +0100

    Update main.js

--- main.js
+++ main.js
@@ -8,13 +8,13 @@ function update(){
 		var iD = mainContext.getImageData(0, 0, 500, 500).data;
 		mainContext.fillStyle = "black";
 		mainContext.fillRect(0, 0, mainCanvas.width, mainCanvas.height);	
-		for(var y=0;y<500;y+=1){
-			for(var x=0;x<500;x+=1){
+		for(var y=0;y<500;y+=5){
+			for(var x=0;x<500;x+=5){
 				var pos = (y * 500 + x) * 4;
 				var luminosity = (0.2126 * iD[pos] + 0.7152 * iD[pos + 1] + 0.0722 * iD[pos + 2]) / 255;
 				var l = 255 * Math.round(luminosity * 1000) / 1000;
 				mainContext.fillStyle = "rgb(" + l + "," + l + "," + l + ")";
-				mainContext.fillRect(x, y, x+1,y+1);
+				mainContext.fillRect(x, y, 1,1);
 			}
 		}
 	}
