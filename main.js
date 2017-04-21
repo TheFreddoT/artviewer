30d0062c0bdcdb12270711a3fbb894502022702c
TheFreddoT <freddytuxworth@gmail.com>
2017-04-21 15:49:15 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ var mean = 0;
 function update(){
 	if(streaming){
 		mainContext.drawImage(camVideo, 0, 0, 500, 500);
+		mainContext.fillStyle = "white";
 		var iD = mainContext.getImageData(0, 0, 500, 500).data;
+		mainContext.fillRect(0, 0, mainCanvas.width, mainCanvas.height);	
 		var oData = "";
 		var total = 0;
 		for(var y=0;y<500;y+=5){
@@ -13,7 +15,8 @@ function update(){
 				var pos = (y * 500 + x) * 4;
 				var luminosity = (0.2126 * iD[pos] + 0.7152 * iD[pos + 1] + 0.0722 * iD[pos + 2]) / 255;
 				var l = Math.round(luminosity * 1000) / 1000;
-				mainContext.fillStyle = "rgba(255, 255, 255, " + l + ")";
+				console.log(l);
+				mainContext.fillStyle = "rgba(0, 0, 0, " + l + ")";
 				mainContext.fillRect(x, y, x+4,y+4);
 			}
 			oData += "\n";
